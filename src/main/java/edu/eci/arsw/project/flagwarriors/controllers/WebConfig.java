deda519723712947a5b763093662fb3aeaa6fc31
Juan Pa fernandez <juanpa20022009@hotmail.com>
2024-11-30 00:34:12 -0500

    implemente el poder, aunque quedan detalles por refactorizar

--- src/main/java/edu/eci/arsw/project/flagwarriors/controllers/WebConfig.java
+++ src/main/java/edu/eci/arsw/project/flagwarriors/controllers/WebConfig.java
@@ -8,19 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    //@Override
+    //public void addCorsMappings(CorsRegistry registry) {
+    //    registry.addMapping("/**")
+    //            .allowedOrigins("http://proyectoflagwarriors-dzbpcyeccmfzgaad.northeurope-01.azurewebsites.net") // Asegúrate de que el frontend esté en este puerto
+    //            .allowedMethods("GET", "POST", "PUT", "DELETE")
+    //            .allowedHeaders("*");
+    //}
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://proyectoflagwarriors-dzbpcyeccmfzgaad.northeurope-01.azurewebsites.net") // Asegúrate de que el frontend esté en este puerto
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+            .allowedOrigins("http://localhost:3000") // Asegúrate de que el frontend esté en este puerto
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*");
     }
-
-    // @Override
-    // public void addCorsMappings(CorsRegistry registry) {
-    //     registry.addMapping("/**")
-    //             .allowedOrigins("http://localhost:3000") // Asegúrate de que el frontend esté en este puerto
-    //             .allowedMethods("GET", "POST", "PUT", "DELETE")
-    //             .allowedHeaders("*");
-    // }
 }
